853152b5eccd6181795efde9576644f2c0d6169f
devmetal <master@devmetal.com.br>
2019-08-20 02:22:23 -0300

    ➕ Statics

---
+++ README.md
@@ -0,0 +1,12 @@
+### Desafio BrasilPrev
+[![Build Status](https://travis-ci.org/devmetalbr/monopyly.svg?branch=master)](https://travis-ci.org/devmetalbr/monopyly)
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg)](https://www.python.org/downloads/release/python-374/)
+
+##### Instalação:
+`git clone https://github.com/devmetalbr/monopyly.git`
+
+`cd monopyly`
+
+`python3 monopyly.py`
+
+[![asciicast](https://asciinema.org/a/ffYl6lvTQvajEMacEWoMuuWWf.svg)](https://asciinema.org/a/ffYl6lvTQvajEMacEWoMuuWWf)
--- wmc/settings.py
+++ wmc/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'apps.website'
 ]
 
 MIDDLEWARE = [
@@ -69,18 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wmc.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -103,9 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
