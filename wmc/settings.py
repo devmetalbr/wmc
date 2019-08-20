dad504cc1d33020942855f12f470eeb08488ed8b
devmetal <master@devmetal.com.br>
2019-08-20 03:34:10 -0300

    Extra places for collectstatic to find static files.

--- wmc/settings.py
+++ wmc/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'wmc', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'wmc', 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
